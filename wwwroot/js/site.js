cc5126d1ea5f43f068d76cf5fe2317bf462b67cf
Administrateur <Administrateur@WIN-OB36CJKPRNR>
2022-07-31 07:14:37 +0200

    Ajoutez des fichiers projet.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+Hello World!
+AddAltToImg = function (imageTagAndImageID, imageContext) {
+    ///<signature>
+    ///<summary> Adds an alt tab to the image
+    // </summary>
+    //<param name="imgElement" type="String">The image selector.</param>
+    //<param name="ContextForImage" type="String">The image context.</param>
+    ///</signature>
+    var imageElement = $(imageTagAndImageID, imageContext);
+    imageElement.attr('alt', imageElement.attr('id').replace(/ID/, ''));
+}
+// Write your JavaScript code.
